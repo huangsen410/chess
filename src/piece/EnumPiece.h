02ffecba9ca00834c7fb3599817632dafc8ebf1e
techgineer <ahdavis13@gmail.com>
2017-01-11 15:43:23 -0600

    Added piece type enum

---
+++ src/piece/EnumPiece.h
@@ -0,0 +1,23 @@
+/*
+ * EnumPiece.h
+ * Enumerates the different kinds of chess pieces
+ * Created by Andrew Davis
+ * Created on 1/11/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef ENUM_PIECE_H
+#define ENUM_PIECE_H
+
+//enum definition
+enum class EnumPiece {
+	PAWN,
+	ROOK,
+	KNIGHT,
+	BISHOP,
+	KING,
+	QUEEN
+};
+
+#endif
