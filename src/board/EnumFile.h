7bf344bc41ca21ffd3f04affd7783a4e52183ea7
techgineer <ahdavis13@gmail.com>
2017-01-14 11:28:22 -0600

    Added chessboard file enum

---
+++ src/board/EnumFile.h
@@ -0,0 +1,25 @@
+/*
+ * EnumFile.h
+ * Enumerates columns on a chessboard
+ * Created by Andrew Davis
+ * Created on 1/14/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef ENUM_FILE_H
+#define ENUM_FILE_H
+
+//enum definition
+enum class EnumFile {
+	A,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H
+};
+
+#endif
